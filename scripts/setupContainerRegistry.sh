b188b86e49308a2e0a70fbcf4f4291b62bb7c631
Johannes Bräuer <johannes.braeuer@dynatrace.com>
2019-06-03 12:54:04 +0200

    #351 Renaming of files

--- scripts/setupContainerRegistry.sh
+++ scripts/setupContainerRegistry.sh
@@ -2,14 +2,14 @@
 
 source ./utils.sh
 
-kubectl apply -f ../manifests/container-registry/k8s-docker-registry-configmap.yml
+kubectl apply -f ../manifests/container-registry/configmap.yml
 verify_kubectl $? "Creating config map for container registry failed."
 
-kubectl apply -f ../manifests/container-registry/k8s-docker-registry-pvc.yml
+kubectl apply -f ../manifests/container-registry/pvc.yml
 verify_kubectl $? "Creating persistent volume claim for container registry failed."
 
-kubectl apply -f ../manifests/container-registry/k8s-docker-registry-deployment.yml
+kubectl apply -f ../manifests/container-registry/deployment.yml
 verify_kubectl $? "Creating deployment for container registry failed."
 
-kubectl apply -f ../manifests/container-registry/k8s-docker-registry-service.yml
+kubectl apply -f ../manifests/container-registry/service.yml
 verify_kubectl $? "Creating service for container registry failed."
