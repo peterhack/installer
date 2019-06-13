7a4dee56a10c29834378c260550e1673682f4612
Andreas Grimmer <andreas.grimmer@dynatrace.com>
2019-06-13 13:38:43 +0200

    #444 Add waits for all deployments

--- scripts/wearUniform.sh
+++ scripts/wearUniform.sh
@@ -10,6 +10,9 @@ verify_kubectl $? "Deploying keptn's uniform failed."
 ##############################################
 wait_for_all_pods_in_namespace "keptn"
 
+wait_for_deployment_in_namespace "gatekeeper-service" "keptn"
+wait_for_deployment_in_namespace "jmeter-service" "keptn"
+wait_for_deployment_in_namespace "helm-service" "keptn"
 wait_for_deployment_in_namespace "github-service" "keptn"
 wait_for_deployment_in_namespace "servicenow-service" "keptn"
 wait_for_deployment_in_namespace "pitometer-service" "keptn"
